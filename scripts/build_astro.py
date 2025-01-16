dc93bb9ce22fc9212d18766147bd3cd49f92dda8
m-kolomanski <m.kolomanski@yahoo.com>
2025-01-16 16:58:29 +0100

    refactor: refactored build_astro, added documentation and tests

--- scripts/build_astro.py
+++ scripts/build_astro.py
@@ -2,54 +2,256 @@
 This script is for building Astro project by parsing the manifest and preparing
 files with pages / albums.
 """
-
 import json
 import yaml
 from datetime import datetime
+from utils import message
 
-with open("manifest.yaml", "r") as f:
-    manifest = yaml.safe_load(f)
+class Album:
+    """
+    A class to represent an album containing games and screenshots.
 
-with open("content.json", "r") as f:
-    content = json.load(f)
+    Attributes:
+    ----------
+    name (str): The name of the album.
+    games (list[str]): A list of games included in the album.
+    screenshots (list[str], optional): A list of screenshots associated with the album (default is an empty list).
 
-with open("web/src/pages/album_template.astro", "r") as f:
-    album_template = f.read()
+    Methods:
+    -------
+    addScreenshot(screenshot):
+        Adds a screenshot to the album.
+    buildAlbumContent():
+        Builds and returns the HTML content for the album's screenshots.
+    buildAlbumCover():
+        Builds and returns the HTML content for the album's cover.
+    buildPageName():
+        Generates and returns a URL-friendly page name based on the album's name.
+    """
+    def __init__(self, name: str, games: list[str], screenshots: list[dict] = None):
+        """
+        Initialize a new instance of the class.
+        Arguments:
+        ----------
+            name (str): The name of the instance.
+            games (str): The games associated with the instance.
+            screenshots (list[dict], optional): A list of screenshots. Defaults to an empty list if not provided.
+        """
+        self.name = name
+        self.games = games
+        self.screenshots = screenshots if screenshots is not None else []
 
-album_covers = []
-games_with_albums = []
+    def addScreenshot(self, screenshot: dict):
+        """
+        Adds a screenshot to the list of screenshots.
+        Arguments:
+        ----------
+            screenshot (dict): The screenshot object to be added to the list.
+        """
+        self.screenshots.append(screenshot)
 
-for album in manifest['albums']:
-    album_content = []
-    for game in content.keys():
-        if game in album['games']:
-            album_content += content[game]
-            games_with_albums.append(game)
+    def buildAlbumContent(self) -> str:
+        """
+        Generates HTML content for an album of screenshots.
+        This method iterates over the `self.screenshots` list, which contains dictionaries
+        with keys "link", "title", and "date". For each screenshot, it creates an HTML
+        string representing an Astro <Screenshot /> component with the corresponding attributes.
 
-    screenshots_html = [f'<Screenshot src="{screenshot["link"]}" alt="{screenshot["title"]}" />' for screenshot in album_content]
+        Returns:
+        ----------
+            str: An HTML string for Astro <AlbumCover />.
+        """
+        screenshots_html = [
+            f'<Screenshot src="{screenshot["link"]}" alt="{screenshot["title"]}" date="{screenshot["date"]}" />'
+            for screenshot in self.screenshots
+        ]
+        return "".join(screenshots_html)
+    
+    def buildAlbumCover(self) -> str:
+        """
+        Generates an HTML string for the Astro <AlbumCover /> component, representing the
+        album cover. 
 
-    album_html = album_template.replace("<!-- SCREENSHOTS -->", "\n".join(screenshots_html))
+        Returns:
+        ----------
+            str: An HTML string for Astro <AlbumCover />.
+        """
+        return f'<AlbumCover name="{self.name}" path="/VirtualMoments/{self.buildPageName()}/" />'
 
-    page_name = album['name'].replace(' ', '_').replace(":", "").replace("'", "").lower()
+    def buildPageName(self) -> str:
+        """
+        Generates a formatted, html-friendly page name by replacing spaces with underscores,
+        removing colons and apostrophes, and converting the string to lowercase.
 
-    album_cover_html = f'<AlbumCover name="{album["name"]}" path="/VirtualMoments/{page_name}/" />'
-    album_covers.append(album_cover_html)
+        Returns:
+        ----------
+            str: The formatted page name.
+        """
 
-    print(page_name)
+        return self.name.replace(' ', '_').replace(":", "").replace("'", "").lower()
+    
+class IndexPage:
+    """
+    A class used to build an index page for a web application.
 
-    with open(f"web/src/pages/{page_name}.astro", "w") as f:
-        f.write(album_html)
+    Attributes:
+    ----------
+    default_page_dir (str): The default directory where the index page will be saved.
+    template (str): The HTML template for the index page.
+    albums (list[Album]): A list of Album objects to be included in the index page.
 
-with open("web/src/pages/index_template.astro", "r") as f:
-    index_html = f.read()
+    Methods:
+    ----------
+    buildIndexPage(save=False, dir=None)
+        Builds the index page by replacing placeholders in the template with album covers and a footer.
+        If save is True, saves the generated HTML to a file in the specified directory or the default directory.
+        If save is False, returns the generated HTML as a string.
+    """
+    default_page_dir = "web/src/pages"
 
-# Add albums to the index page #
-index_html = index_html.replace("<!-- ALBUMS -->", "\n".join(album_covers))
+    def __init__(self, template: str, albums: list[Album]):
+        """
+        Initializes the instance with a template and a list of albums.
+        Arguments:
+        ----------
+            template (str): The template string to be used.
+            albums (list[Album]): A list of Album objects.
+        """
+        self.template = template
+        self.albums = albums
 
-# Add update date to the footer #
-date = datetime.now().strftime("%B %Y")
-index_html = index_html.replace("<!-- FOOTER -->", f'<Footer date="{date}" />')
+    def buildIndexPage(self, save: bool = False, dir: str = None) -> str:
+        """
+        Builds the index page for the album collection.
+        This method generates the HTML content for the index page by replacing
+        placeholders in the template with the album covers and the current date
+        in the footer. The generated HTML can either be saved to a file or returned
+        as a string.
 
+        Arguments:
+        ----------
+            save (bool): If True, the generated HTML will be saved to a file.
+                         If False, the generated HTML will be returned as a string.
+                         Default is False.
+            dir (str, optional): The directory where the index page will be saved.
+                                 If not provided, the default directory will be used.
 
-with open("web/src/pages/index.astro", "w") as f:
-    f.write(index_html)
+        Returns:
+        ----------
+            str: The generated HTML content of the index page if `save` is False.
+        """
+        index_html = self.template.replace(
+            "<!-- ALBUMS -->",
+            "".join([album.buildAlbumCover() for album in self.albums])
+        )
+
+        index_html = index_html.replace(
+            "<!-- FOOTER -->",
+            f'<Footer date="{datetime.now().strftime("%d %B %Y")}" />'
+        )
+
+        if save:
+            if dir is None: dir = self.default_page_dir
+
+            with open(f"{dir}/index.astro", "w", encoding="utf-8") as f:
+                f.write(index_html)
+        else:
+            return index_html
+
+class AlbumPage:
+    """
+    A class used to represent an Album Page.
+
+    Attributes:
+    ----------
+    default_page_dir (str): The default directory where the album page will be saved.
+    template (str): The HTML template for the album page.
+    album (Album): An instance of the Album class containing album data.
+
+    Methods:
+    ----------
+    buildAlbumPage(save=False, dir=None):
+        Builds the album page HTML. If save is True, saves the HTML to a file in the specified directory.
+    """
+    default_page_dir = "web/src/pages"
+
+    def __init__(self, template: str, album: Album):
+        """
+        Initializes the BuildAstro class with the given template and album.
+        Arguments:
+        ----------
+            template (str): The template string to be used.
+            album (Album): An instance of the Album class.
+        """
+        self.template = template
+        self.album = album
+
+    def buildAlbumPage(self, save: bool = False, dir: str = None):
+        """
+        Builds the HTML content for an album page.
+        This method generates the HTML content for the album page by replacing
+        placeholders in the template with the album content HTML. The generated
+        HTML text can either be saved to a file or returned as a string.
+
+        Arguments:
+        ----------
+            save (bool): If True, the generated HTML content will be saved to a file. Defaults to False.
+            dir (str, optional): The directory where the file should be saved.
+                                 If None, the default directory will be used. Defaults to None.
+
+        Returns:
+        ----------
+            str: The generated HTML content if `save` is False.
+        """
+        album_html = self.template.replace("<!-- SCREENSHOTS -->", self.album.buildAlbumContent())
+
+        if save:
+            if dir is None: dir = self.default_page_dir
+
+            with open(f"{dir}/{self.album.buildPageName()}.astro", "w", encoding="utf-8") as f:
+                f.write(album_html)
+        else:
+            return album_html
+
+if __name__ == "__main__":
+    message("Building Astro project...")
+    message("Loading files...")
+    with open("manifest.yaml", "r") as f:
+        manifest = yaml.safe_load(f)
+
+    with open("content.json", "r") as f:
+        content = json.load(f)
+
+    with open("web/src/pages/album_template.astro", "r") as f:
+        album_template = f.read()
+
+    with open("web/src/pages/index_template.astro", "r") as f:
+        index_template = f.read()  
+
+    message("Creating Albums...")
+    albums = [Album(album['name'], album['games']) for album in manifest['albums']]
+    albums.append(Album("Other", []))
+
+    for screenshot in content:
+        has_album = False
+
+        for album in albums:
+            if screenshot['game'] in album.games:
+                has_album = True
+                album.addScreenshot(screenshot)
+
+        if has_album:
+            continue
+        else:
+            albums[-1].addScreenshot(screenshot)
+
+    message("Bulding pages...")
+    index_page = IndexPage(index_template, albums)
+    album_pages = [AlbumPage(album_template, album) for album in albums]
+
+    for album_page in album_pages:
+        album_page.buildAlbumPage(save = True)
+
+    index_page.buildIndexPage(save = True)
+
+    message(f"Building complete for {len(album_pages)} albums.")
---
+++ tests/pytest/test_build_astro.py
@@ -0,0 +1,138 @@
+import sys
+from pathlib import Path
+# Add the run directory to the path, othwerwise pytests
+# fails to find the tested file.
+script_path = Path(__file__).resolve().parent.parent.parent / "scripts"
+sys.path.insert(0, str(script_path))
+
+import pytest
+from datetime import datetime
+from build_astro import Album, IndexPage, AlbumPage
+
+@pytest.fixture
+def album():
+    return Album(
+            name = "Test album", 
+            games = ["Game 1", "Game 2"],
+            screenshots = [
+                {"game": "Game 1", "title": "Test title 1", "link": "https://test-link1.com", "date": "21 January 2024"},
+                {"game": "Game 2", "title": "Test title 2", "link": "https://test-link2.com", "date": "25 January 2025"}
+            ]
+        )
+
+class TestAlbum:
+    def test_createAlbum(self):
+        album = Album("Test album", ["Game 1", "Game 2"])
+
+        assert album.name == "Test album"
+        assert album.games == ["Game 1", "Game 2"]
+        assert album.screenshots == []
+
+    def test_addScreenshot(self, album):
+        screenshot = {
+            "game": "Game 1",
+            "title": "Test title",
+            "link": "https://test-link.com",
+            "date": "21 January 2024"
+        }
+
+        album.addScreenshot(screenshot)
+
+        assert len(album.screenshots) == 3
+        assert album.screenshots[2] == screenshot
+
+    def test_buildAlbumContent(self, album):
+        album_content = album.buildAlbumContent()
+
+        assert type(album_content) == str
+        assert '<Screenshot src="https://test-link1.com" alt="Test title 1" date="21 January 2024" />' in album_content
+        assert '<Screenshot src="https://test-link2.com" alt="Test title 2" date="25 January 2025" />' in album_content
+
+    def test_buildAlbumCover(self, album):
+        album_cover = album.buildAlbumCover()
+
+        assert album_cover == '<AlbumCover name="Test album" path="/VirtualMoments/test_album/" />'
+
+    def test_buildPageName(self, album):
+        page_name1 = album.buildPageName()
+        assert page_name1 == "test_album"
+
+        album2 = Album("ThiS is A'n: Album", [])
+        page_name2 = album2.buildPageName()
+        assert page_name2 == "this_is_an_album"
+
+class TestIndexPage:
+    @pytest.fixture
+    def index_page(self, album):
+        template = """
+            <html>
+                <body>
+                    <!-- ALBUMS -->
+                    <!-- FOOTER -->
+                </body>
+            </html>
+        """
+        return IndexPage(template, [album])
+    
+    @pytest.fixture
+    def expected_html(self):
+        return f"""
+            <html>
+                <body>
+                    <AlbumCover name="Test album" path="/VirtualMoments/test_album/" />
+                    <Footer date="{datetime.now().strftime("%d %B %Y")}" />
+                </body>
+            </html>
+        """
+
+    def test_buildIndexPage(self, index_page, expected_html):
+        index_html = index_page.buildIndexPage()
+
+        assert type(index_html) == str
+        assert expected_html == index_html
+
+    def test_buildIndexPage_save(self, index_page, tmp_path, expected_html):
+        index_page.buildIndexPage(save=True, dir=tmp_path)
+
+        index_file = tmp_path / "index.astro"
+        assert index_file.exists()
+
+        with open(index_file, "r", encoding="utf-8") as f:
+            index_html = f.read()
+
+        assert expected_html == index_html
+
+class TestAlbumPage:
+    @pytest.fixture
+    def album_page(self, album):
+        template = """
+            <html>
+                <body>
+                    <!-- SCREENSHOTS -->
+                </body>
+            </html>
+        """
+        return AlbumPage(template, album)
+    
+    @pytest.fixture
+    def expected_html(self):
+        return '<Screenshot src="https://test-link1.com" alt="Test title 1" date="21 January 2024" />'
+
+    def test_buildAlbumPage(self, album_page, expected_html):
+        album_html = album_page.buildAlbumPage()
+
+        assert type(album_html) == str
+        assert '<Screenshot src="https://test-link1.com" alt="Test title 1" date="21 January 2024" />' in album_html
+        assert '<Screenshot src="https://test-link2.com" alt="Test title 2" date="25 January 2025" />' in album_html
+
+    def test_buildAlbumPage_save(self, album_page, tmp_path):
+        album_page.buildAlbumPage(save=True, dir=tmp_path)
+
+        album_file = tmp_path / "test_album.astro"
+        assert album_file.exists()
+
+        with open(album_file, "r", encoding="utf-8") as f:
+            album_html = f.read()
+
+        assert '<Screenshot src="https://test-link1.com" alt="Test title 1" date="21 January 2024" />' in album_html
+        assert '<Screenshot src="https://test-link2.com" alt="Test title 2" date="25 January 2025" />' in album_html
